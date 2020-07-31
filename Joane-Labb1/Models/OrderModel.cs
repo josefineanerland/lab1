d34ac44ce50c5abc9c847683b299dc2ed332b651
Josefine <josefineanerland@gmail.com>
2020-07-31 15:12:01 +0200

    Added Orders

---
+++ Joane-Labb1/Models/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Joane_Labb1.Models
+{
+    public class OrderModel
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Adress { get; set; }
+        public int PostalCode { get; set; }
+        public string City { get; set;
+        }
+    }
+}

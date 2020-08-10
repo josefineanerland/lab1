5b40a3117d387c6e57a956cca0e3c2ad6b39f18e
Josefine Anerland <jossan-a-h@hotmail.com>
2020-08-10 18:18:18 +0200

    Insert Products-script

---
+++ Joane-Labb1/InsertProducts.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO [dbo].[ProductModel]
+           ([Id],[ProductName],[Description],[Price],[ImageUrl])
+     VALUES
+           (NEWID(),'Falselash G800','Falselash G800', '99.00', 'https://media.fyndiq.se/product/33/99/cf/3f88f033e16eb2edde3c5373033460e486/original.png'),
+		   (NEWID(),'Single lashes','Single lashes for extensions, no glue included', '159.00', 'https://media.fyndiq.se/product/6b/0a/fe/393d51460b4cda8c74677545dad799b33a/original.png'),
+			(NEWID(),'Falselash faux mink','Falselash G320, faux mink', '120.00', 'https://media.fyndiq.se/product/f1/f6/92/913cbbe2b21bbdee79cddff93fc22b7633/original.png')
+
+GO
+

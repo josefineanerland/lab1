e39e090620ca4ecc76680c6a15d656e3150222e3
Josefine Anerland <jossan-a-h@hotmail.com>
2020-08-21 12:04:36 +0200

    API-project with model and dbcontext

---
+++ OrderServices/DbContext/OrderContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Model;
+
+namespace OrderService.DBContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+
+
+    }
+}

f927d2175fa48ef52c20fe34eb54c435721ebf95
Josefine Anerland <jossan-a-h@hotmail.com>
2020-08-21 19:11:58 +0200

    Model, DbContext, Connectionstring

---
+++ ProductService/DbContext/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Model;
+
+namespace OrderService.DBContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Orders { get; set; }
+
+
+    }
+}
